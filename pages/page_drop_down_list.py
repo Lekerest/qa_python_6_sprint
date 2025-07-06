6d41b5cda31f37a840486f1b46b744d23c7dff8c
Иван <92312258+Lekerest@users.noreply.github.com>
2025-07-06 15:08:49 +0700

    Update page_drop_down_list.py

--- pages/page_drop_down_list.py
+++ pages/page_drop_down_list.py
@@ -3,11 +3,11 @@ from pages.base_page import BasePage
 
 
 class PageDropDownList(BasePage):
-
-    @allure.step('Нажимаем на вопрос')
-    def click_on_the_question(self, locator):
-        return self.click_element(locator)
-
+    
     @allure.step('Получаем текст FAQ')
     def get_text_faq(self, locator):
         return self.find_element(locator).text
+    
+    @allure.step('Нажимаем на вопрос')
+    def click_on_the_question(self, locator):
+        return self.click_element(locator)
